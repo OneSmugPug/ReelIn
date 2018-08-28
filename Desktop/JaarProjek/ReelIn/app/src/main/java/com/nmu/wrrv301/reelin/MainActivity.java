9e0ca16ad783f0a26949b6e56dc25670b487d554
Heine <heine.vorster@gmail.com>
2018-08-28 20:18:52 +0200

    heine

---
+++ Desktop/JaarProjek/ReelIn/app/src/main/java/com/nmu/wrrv301/reelin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nmu.wrrv301.reelin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
